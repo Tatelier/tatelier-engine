c21e1606b4891e9560f6813329462be34fe1a69f
Pansystar <wh.bt3.p@gmail.com>
2022-02-04 09:53:09 +0900

    モジュールDLスクリプトを修正

--- src/submodules/submodule-download.ps1
+++ src/submodules/submodule-download.ps1
@@ -1,7 +1,11 @@
-﻿$now = Get-Date -Format "hhmmss"
-$DxLibCSDLLZipUrl = "https://avatars.githubusercontent.com/u/91860836?v=4"
+﻿Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+$now = Get-Date -Format "hhmmss"
+$DxLibCSDLLZipUrl = "https://dxlib.xsrv.jp/DxLib/DxLibDotNet3_23.zip"
 $DxLibCSDllZipFilePath = "$env:TEMP\dxlib.zip"
 $DxLibCSDllZipExpandFolder = "$env:TEMP\dxlib_$now"
+
+
 
 Invoke-WebRequest -Uri $DxLibCSDLLZipUrl -OutFile $DxLibCSDllZipFilePath
 
